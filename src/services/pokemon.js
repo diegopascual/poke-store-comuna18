271571a970ddcc29160ccf8bd929c97640b98f0a
Diego Pascual <diegopascualh@outlook.com>
2025-08-15 17:55:10 -0600

    feat: Add local storage to save pokemon

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,8 +1,16 @@
 import { getPokemonList as getPokemonListApi } from "@/api/pokemon";
 import { fetchDataFromUrl } from "@/api/generic";
+import { saveToStorage, loadFromStorage } from "@/utils/storage";
 import { mapApiResponseToPokemon } from "./helpers";
 
 export const getPokemonList = async ({ limit = 20, page = 1 }) => {
+  const storageKey = `pokemon-list-page-${page}`;
+  const cachedData = loadFromStorage(storageKey);
+  if (cachedData) {
+    console.log("Serving from localStorage cache ✨");
+    return cachedData;
+  }
+
   const pokemonListResponse = await getPokemonListApi({
     limit,
     offset: (page - 1) * limit,
@@ -14,6 +22,8 @@ export const getPokemonList = async ({ limit = 20, page = 1 }) => {
 
   const pokemonResponses = await Promise.all(pokemonPromises);
   const pokemonResults = pokemonResponses.map(mapApiResponseToPokemon);
+
+  saveToStorage(storageKey, pokemonResults);
 
   return pokemonResults;
 };
---
+++ src/utils/storage.js
@@ -0,0 +1,23 @@
+export const saveToStorage = (key, value) => {
+  try {
+    const serializedValue = JSON.stringify(value);
+    localStorage.setItem(key, serializedValue);
+  } catch (error) {
+    console.error("Error saving to localStorage", error);
+  }
+};
+
+export const loadFromStorage = (key) => {
+  try {
+    const serializedValue = localStorage.getItem(key);
+
+    if (serializedValue === null) {
+      return undefined;
+    }
+
+    return JSON.parse(serializedValue);
+  } catch (error) {
+    console.error("Error loading from localStorage", error);
+    return undefined;
+  }
+};
